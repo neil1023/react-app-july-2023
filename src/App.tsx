0096cd44d197ce9d73d53eee0aa4a4471ca68083
Neil Patel <neil1023@yahoo.com>
2023-07-04 15:47:11 -0400

    Displayed alert on click and removed Alert on close

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Alert } from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  let [alertVisibility, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      {alertVisibility && (
+        <Alert onCloseButtonClicked={() => setAlertVisibility(false)}>
+          My Alert
+        </Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,13 +1,24 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onCloseButtonClicked: MouseEventHandler;
 }
 
-export const Alert = ({ children }: Props) => {
+export const Alert = ({ children, onCloseButtonClicked }: Props) => {
   return (
-    <div className="alert alert-primary" role="alert">
+    <div
+      className="alert alert-primary alert-dismissable fade show"
+      role="alert"
+    >
       {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onCloseButtonClicked}
+      ></button>
     </div>
   );
 };
