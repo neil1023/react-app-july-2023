2e11722ef38a9b906de6c1e7e97d17b279731d37
Neil Patel <neil1023@yahoo.com>
2023-07-04 12:56:38 -0400

    Displaying Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import { Alert } from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      <Alert text="Hello World" />
     </div>
   );
 }
